bacb9707a671e0d18e244fa3e268d5c023800d79
Anshul <anshuldhakar03@gmail.com>
2023-11-07 18:28:07 +0530

    Initial commit

---
+++ apps/react-todo-v1/src/app/app.tsx
@@ -0,0 +1,14 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import styles from './app.module.css';
+
+import NxWelcome from './nx-welcome';
+
+export function App() {
+  return (
+    <div>
+      <NxWelcome title="react-todo-v1" />
+    </div>
+  );
+}
+
+export default App;
